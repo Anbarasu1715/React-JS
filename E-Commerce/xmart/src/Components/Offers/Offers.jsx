40b5a1d5ba2b10cc08377f3464fce2abb3302473
Anbarasu <anbarasu47442003@gmail.com>
2024-09-06 21:02:26 +0530

    XMart by React.js

--- E-Commerce/xmart/src/Components/Offers/Offers.jsx
+++ E-Commerce/xmart/src/Components/Offers/Offers.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Offers.css'
+import elx_img from '../Assets/exclusive_image.png'
 
 const Offers = () => {
   return (
     <div className='Offer'>
-
+        <div className="Offer-left">
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p>Only On Best Seller Products</p>
+            <button>Check Now</button>
+        </div>
+        <div className="Offer-right">
+            <img src={elx_img} alt="Exclusive" />
+        </div>
     </div>
   )
 }
